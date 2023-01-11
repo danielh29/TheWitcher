bd7b0505c7b91c1b3d43c3e2ad6891b6cb471f22
Daniel <danhmine883@gmail.com>
2023-01-11 12:40:02 +0100

    añadidos atributos

--- src/witcher/Witcher.java
+++ src/witcher/Witcher.java
@@ -1,9 +1,15 @@
 package witcher;
 
 public class Witcher {
+	
+	private String name;
+	private int health;
+	private boolean hasSoul;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
 
 	}
 
